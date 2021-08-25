4830f5b66c4559f98f3fe902e2a7b7f82204f0a3
Éverton Victor <evertonvictor2019br@gmail.com>
2021-08-25 19:11:59 -0300

    Finalizado
    Foi criado a função calcular, e suas operações

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 const calcular = (tipo, valor) => {
+    if(tipo === 'acao') {
+        switch(valor) {
+            case 'c':
+                document.getElementById('resultado').value = ''
+                break
 
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '.':
+                document.getElementById('resultado').value += valor
+                break
+            
+            case '=':
+                var valor_campo = document.getElementById('resultado').value
+                document.getElementById('resultado').value = eval(valor_campo)
+                break
+            
+            default:
+                document.getElementById('resultado').value = 0;
+                break
+        }
+    }else if(tipo === 'numero') {
+        document.getElementById("resultado").value += valor
+    }else {
+        document.getElementById('resultado').value = 0;
+    }
 }
